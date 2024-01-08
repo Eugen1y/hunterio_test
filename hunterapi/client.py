31a96ef2e0db29f41118ae765a93338ed06c9d2d
Evgeniy Kolodko <e.kolodko@intetics.com>
2024-01-08 18:56:25 +0200

    reworked request logic

--- hunterapi/client.py
+++ hunterapi/client.py
@@ -8,9 +8,6 @@ import requests
 class HunterClient(object):
     """Class for interacting with the hunter.io API."""
 
-    base_url = 'https://api.hunter.io/v2/'
-    status_ok = 200
-
     def __init__(self, api_key: str):
         """Initialize the HunterClient with the specified API key.
 
@@ -18,8 +15,41 @@ class HunterClient(object):
         :type: str
         """
         self.api_key = api_key
+        self.base_params = {'api_key': api_key}
+        self.base_endpoint = 'https://api.hunter.io/v2/'
 
-    def verify_email(self, email: str) -> Dict[str, Any]:
+    def make_endpoint(self, endpoint: str) -> str:
+        """Make endpoint address.
+
+        :param endpoint: str
+        :return: endpoint address
+        """
+        return self.base_endpoint + endpoint
+
+    def make_request(self, endpoint: str, request_params: dict, request_type='get', raw=False) -> Dict[str, Any]:
+        """Make a request to the hunter.io API.
+
+        :param endpoint: endpoint to send request
+        :param params: parameters to request
+        :param raw: Gives back the entire response instead of just the 'data'.
+
+        :return:
+        """
+        request_kwargs = {'params': request_params}
+        res = getattr(requests, request_type)(endpoint, **request_kwargs)
+        res.raise_for_status()
+
+        if raw:
+            return res
+
+        try:
+            result_data = res.json()['data']
+        except KeyError:
+            raise ValueError(res.json())
+
+        return result_data
+
+    def verify_email(self, email: str, raw=False) -> Dict[str, Any]:
         """Verify the specified email address using the hunter.io API.
 
         :param email: The email address to be verified.
@@ -28,33 +58,29 @@ class HunterClient(object):
         :return: A dictionary containing the verification results.
         :rtype: Dict[str, Any]
 
-        raises ValueError: If the API key or email is incorrect.
+        :raises requests.RequestException: If there was an issue with the request.
 
         """
-        endpoint = 'email-verifier?email={email}&api_key={api_key}'.format(email=email, api_key=self.api_key)
-        response = requests.get(self.base_url + endpoint, timeout=2)
+        request_params = {'email': email, 'api_key': self.api_key}
+        endpoint = self.make_endpoint('email-verifier')
+        return self.make_request(endpoint, request_params, raw=raw)
 
-        if response.status_code == self.status_ok:
-            return response.json()
-
-        return response.json()['errors'][0]['details']
-
-    def get_domain_emails(self, domain: str) -> Dict[str, Any]:
-        """Retrieve data about domain emails using the hunter.io API.
+    def get_domain_emails(self, domain: str, company: str, raw=False) -> Dict[str, Any]:
+        """Retrieve the number of email addresses Hunter has for this domain/company.
 
         :param domain: The domain address to check
-        :type: str
+
+        :param company: The name of company to check
 
         :return: A dictionary containing emails data.
-        :rtype: Dict[str, Any]
 
-        raises ValueError: If the domain is missing
+        :raises requests.RequestException: If there was an issue with the request.
 
         """
-        endpoint = 'email-count?domain={domain}'.format(domain=domain)
-        response = requests.get(self.base_url + endpoint, timeout=2)
-
-        if response.status_code == self.status_ok:
-            return response.json()
-
-        return response.json()['errors'][0]['details']
+        request_params = self.base_params
+        if domain:
+            request_params['domain'] = domain
+        elif company:
+            request_params['company'] = company
+        endpoint = self.make_endpoint('email-count')
+        return self.make_request(endpoint, request_params, raw=raw)
